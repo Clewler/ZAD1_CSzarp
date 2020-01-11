70e2cf4ef6150c90dc70a71e1f4d7acc732e0592
Dawid Brylka <dawid.brylka@tieto.com>
2020-01-11 16:55:08 +0100

    Dodaj pliki projektów.

---
+++ ZAD1_CSzarp/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace ZAD1_CSzarp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.wtyczkiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 68);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(768, 348);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(94, 39);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.wtyczkiToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // wtyczkiToolStripMenuItem
+            // 
+            this.wtyczkiToolStripMenuItem.Name = "wtyczkiToolStripMenuItem";
+            this.wtyczkiToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.wtyczkiToolStripMenuItem.Text = "abc";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(175, 39);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(256, 41);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 507);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem wtyczkiToolStripMenuItem;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ ZAD1_CSzarp/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+using System.Reflection;
+
+
+namespace ZAD1_CSzarp
+{
+    public partial class Form1 : Form
+    {
+        void LoadPlugins()
+        {
+            foreach (string myFilename in Directory.GetFiles(@"C:\Root", "*.dll", SearchOption.AllDirectories))
+            {
+                Assembly plugin = Assembly.LoadFrom(myFilename);
+                foreach (Type item in plugin.GetTypes()) //lista klas
+                    foreach (MethodInfo method in item.GetMethods())
+                    {
+                        
+                        wtyczkiToolStripMenuItem.DropDownItems.Add(new ToolStripMenuItem(method.Name, null,
+MenuHandler, myFilename + "|" + item.Name + "|" + method.Name));
+
+                    }
+
+            }
+        }
+
+        private void MenuHandler(object sender, EventArgs e)
+        {
+            ToolStripMenuItem item = (ToolStripMenuItem)sender;
+            string[] names = item.Name.Split(new char[] { '|' });
+            string NazwaAssembly = names[0];
+            string NazwaKlasy = names[1];
+            string NazwaMetody = names[2];
+            Assembly plugin = Assembly.LoadFile(NazwaAssembly);
+            MethodInfo method;
+            foreach (Type item1 in plugin.GetTypes())
+            {
+                method = item1.GetMethod(NazwaMetody);
+                object result;
+                try
+                {
+                    result = method.Invoke(null, new object[] { });
+                    method = item1.GetMethod(result.ToString().Substring(1));
+                }
+                catch
+                {
+                    continue;
+                }
+                if (result.ToString().Substring(0, 1) == "r")
+                {
+                    method.Invoke(null, new object[] { richTextBox1 });
+                }
+                else if(result.ToString().Substring(0, 1).Equals("s")){
+                        richTextBox1.Text = method.Invoke(null, new object[] { result.ToString().Substring(1) }).ToString();
+                }
+                else if (result.ToString().Substring(0, 1).Equals("e"))
+                {
+                    method.Invoke(null, new object[] { result.ToString().Substring(1) });
+                }
+                break;
+
+
+            }
+            
+
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadPlugins();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFile1 = new SaveFileDialog();
+            saveFile1.DefaultExt = "*.rtf";
+            saveFile1.Filter = "RTF Files|*.rtf";
+            if (saveFile1.ShowDialog() == System.Windows.Forms.DialogResult.OK &&
+            saveFile1.FileName.Length > 0)
+            {
+                richTextBox1.SaveFile(saveFile1.FileName);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile1 = new OpenFileDialog();
+            openFile1.DefaultExt = "*.rtf";
+            openFile1.Filter = "RTF Files|*.rtf";
+            if (openFile1.ShowDialog() == System.Windows.Forms.DialogResult.OK &&
+            openFile1.FileName.Length > 0)
+            {
+                richTextBox1.LoadFile(openFile1.FileName);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (!wtyczkiToolStripMenuItem.DropDownItems.ContainsKey(textBox1.Text))
+            {
+                wtyczkiToolStripMenuItem.DropDownItems.Add(new ToolStripMenuItem(textBox1.Text,
+ null, MenuHandler, textBox1.Text));
+            }
+
+        }
+    }
+}
